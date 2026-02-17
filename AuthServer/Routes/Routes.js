03bcc41ba4036aca15795c14388ab48fbf069d56
Roop Teja <roopteja52@gmail.com>
2026-02-17 08:18:03 +0530

    Add: Update Logic added

--- AuthServer/Routes/Routes.js
+++ AuthServer/Routes/Routes.js
@@ -3,11 +3,15 @@ const AuthRouter = express.Router();
 const { register, login, validateUser, verifyOTP, UpdateEmail, ChangePassword } = require("../Controller/Controller");
 const { AuthMiddleware } = require("../Middleware/Middleware");
 
+// Post Routes
 AuthRouter.post("/register", register);
 AuthRouter.post("/login", login);
 AuthRouter.post("/validate-user", validateUser);
 AuthRouter.post("/verify-otp", verifyOTP);
-AuthRouter.post("/update-email", AuthMiddleware, UpdateEmail);
-AuthRouter.post("/change-password", AuthMiddleware, ChangePassword);
+
+// Update Routes
+AuthRouter.patch("/update-email", AuthMiddleware, UpdateEmail);
+AuthRouter.patch("/change-password", AuthMiddleware, ChangePassword);
+AuthRouter.patch("/update-profile", AuthMiddleware, updateProfile);
 
 module.exports = AuthRouter;
