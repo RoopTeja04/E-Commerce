393ad5e7210c6a4ec52c7dc800cddb7197886606
Roop Teja <roopteja52@gmail.com>
2026-03-15 15:14:19 +0530

    Upd: Added few more opertaions in backend

--- ProductsServer/Controller/Products.js
+++ ProductsServer/Controller/Products.js
@@ -26,3 +26,81 @@ exports.addProductsInBulk = async (req, res) => {
         });
     }
 }
+
+
+exports.findItemsByCategory = async (req, res) => {
+
+    const { category, productName } = req.query;
+
+    try {
+
+        if (!category && !productName) {
+            return res.status(400).json({
+                message: "Please provide category or productName",
+            });
+        };
+
+        let query = {};
+
+        if (category && category !== "all") {
+            query.category = { $regex: category, $options: "i" }
+        };
+
+        if (productName) {
+            query.name = { $regex: productName, $options: "i" }
+        };
+
+        const FetchProducts = await Products.find(query);
+
+        if (FetchProducts.length === 0) {
+            return res.status(404).json({
+                message: "No products found",
+            });
+        };
+
+        return res.status(201).json({
+            message: "Products fetched successfully",
+            totalLength: FetchProducts.length,
+            product: FetchProducts,
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to fetch products",
+            error: error.message
+        });
+    }
+}
+
+exports.findItemsById = async (req, res) => {
+
+    const ProductId = req.params.ProductId;
+
+    try {
+
+        if(!ProductId){
+            return res.status(400).json({
+                message: "Please provide ProductId",
+            });
+        }
+
+        const FetchProduct = await Products.findById(ProductId);
+
+        if(!FetchProduct){
+            return res.status(404).json({
+                message: "No product found",
+            });
+        }
+
+        return res.status(201).json({
+            message: "Product fetched successfully",
+            product: FetchProduct,
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to fetch products",
+            error: error.message
+        });
+    }
+}
--- ProductsServer/Routes/ProductRoutes.js
+++ ProductsServer/Routes/ProductRoutes.js
@@ -1,6 +1,11 @@
 const Router = require("express").Router();
-const { addProductsInBulk } = require("../Controller/Products");
+const { addProductsInBulk, findItemsByCategory, findItemsById } = require("../Controller/Products");
 
+// Add products in bulk
 Router.post("/add-products-in-bulk", addProductsInBulk);
+
+// Get Products by Search
+Router.get("/get-products-by-search", findItemsByCategory);
+Router.get("/get-products-by-id/:ProductId", findItemsById);
 
 module.exports = Router;
